4a4abdecc6fd143b876141a2bc8439bec82866dc
Freddy R <frondonfermin@gmail.com>
2018-04-11 18:10:58 -0400

    Añadir scaffolded

---
+++ Final/Controllers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Final.Data;
+using Final.Models.Entities;
+
+namespace Final.Controllers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Final.Data.ApplicationDbContext _context;
+
+        public IndexModel(Final.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products.ToListAsync();
+        }
+    }
+}
